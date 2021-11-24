175f99ca36cf0cca73aa1eba07a5851980cb81f7
Nicolas Miranda <nicolasformal415@gmail.com>
2021-11-24 20:25:11 -0300

    login y registro agregado html

--- solution/Cabana.Web/Controllers/HomeController.cs
+++ solution/Cabana.Web/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace Cabana.Web.Controllers
             return View();
         }
 
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ solution/Cabana.Web/Controllers/RegistrarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cabana.Web.Controllers
+{
+    public class RegistrarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
