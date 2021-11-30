f242e65d08a46e006825cf5b98123e08042dbc38
Nicolas Miranda <nicolasformal415@gmail.com>
2021-11-30 19:00:07 -0300

    completando arriendo

--- solution/Cabana.Web/Controllers/ArriendoController.cs
+++ solution/Cabana.Web/Controllers/ArriendoController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cabana.BLL.Arriendo.BusinessImplement;
+using Cabana.BLL.Arriendo.BusinessInteface;
+using Cabana.BLL.Arriendo.DataTransferObject;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cabana.Web.Controllers
 {
     public class ArriendoController : Controller
     {
+        private readonly ICabanaService _cabanaService;
+        public ArriendoController()
+        {
+            _cabanaService = new CabanaServiceImpl();
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Arrendar(IFormCollection collection)
+        {
+            var cabanaDto = new CabanaDto
+            {
+                CabanaId = collection[""],
+                ModeloCabanaId=collection[""],
+                NumeroCabana=collection[""],
+                FechaCreacion = DateTime.Now,
+                UsuarioCreacion = "admin"
+            };
         }
     }
 }
