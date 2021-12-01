a1f9d9f6658004085d7915a98d59a6512537afa0
Nicolas Miranda <nicolasformal415@gmail.com>
2021-12-01 19:06:48 -0300

    view arriendo mod

--- solution/Cabana.Web/Controllers/ArriendoController.cs
+++ solution/Cabana.Web/Controllers/ArriendoController.cs
@@ -16,17 +16,17 @@ namespace Cabana.Web.Controllers
         {
             return View();
         }
-        [HttpPost]
-        public IActionResult Arrendar(IFormCollection collection)
+        //[HttpPost]
+       /* public IActionResult Arrendar(IFormCollection collection)
         {
-            var cabanaDto = new CabanaDto //d
+            var cabanaDto = new CabanaDto
             {
-                CabanaId = collection[""],,
-                ModeloCabanaId=collection[""],,
-                NumeroCabana=collection[""],,
+                CabanaId = collection[""],
+                ModeloCabanaId=collection[""],
+                NumeroCabana=collection[""],
                 FechaCreacion = DateTime.Now,
-                UsuarioCreacion = "admin",
+                UsuarioCreacion = "admin"
             };
-        }
+        }*/
     }
 }
