36319449ce4015e27c05cc15b8abacf5610af2e5
Nicolas Miranda <nicolasformal415@gmail.com>
2021-12-09 16:21:53 -0300

    HACIENDO LIST

--- solution/Cabana.Web/Controllers/ArriendoController.cs
+++ solution/Cabana.Web/Controllers/ArriendoController.cs
@@ -1,6 +1,8 @@
 ﻿using Cabana.BLL.Arriendo.BusinessImplement;
 using Cabana.BLL.Arriendo.BusinessInteface;
 using Cabana.BLL.Arriendo.DataTransferObject;
+using Cabana.BLL.Arriendo.Implementation;
+using Cabana.BLL.Arriendo.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cabana.Web.Controllers
@@ -8,6 +10,7 @@ namespace Cabana.Web.Controllers
     public class ArriendoController : Controller
     {
         private readonly ICabanaService _cabanaService;
+        private readonly IArriendoService _arriendoService;
         public ArriendoController()
         {
             _cabanaService = new CabanaServiceImpl();
@@ -20,6 +23,12 @@ namespace Cabana.Web.Controllers
         public ActionResult ResultadoArriendoDto()
         {
             return View("View");
+        }
+       
+        public ActionResult ResultadoBusquedaView()
+        {
+            _arriendoService = new ArriendoServiceImpl();
+            return View(ResultadoBusquedaView);
         }
     }
 }
